d78a72eb50eef76120e4b4805fa1e9cd6a89a9d4
Riccardo Cavallin <riccardocavallin14@gmail.com>
2020-08-20 18:16:24 +0200

    minting and burning tokens

--- src/index.js
+++ src/index.js
@@ -19,41 +19,41 @@ const faucetAddress = 'JH1P8f3znbyrDj8F4RWpix7hRkgxqHjdW2fNnKpR3v6ufXnknor'
 const faucetAccount = RadixAccount.fromAddress(faucetAddress, true)
 const message = 'Dear Faucet, may I please have some money?'
 
-  RadixTransactionBuilder
-    .createRadixMessageAtom(myAccount, faucetAccount, message)
-    .signAndSubmit(myIdentity)
+RadixTransactionBuilder
+  .createRadixMessageAtom(myAccount, faucetAccount, message)
+  .signAndSubmit(myIdentity)
 
 
-  const radixToken = radixUniverse.nativeToken  
-  myAccount.transferSystem.getTokenUnitsBalanceUpdates().subscribe(balance => {
-    // Get the balance for the token we are interested in
-    const nativeTokenBalance = balance[radixToken.toString()]
-    console.log("nativeTokenBalance: " + nativeTokenBalance)
-    // do we have at least 5 tokens?
-    if (nativeTokenBalance.greaterThan(5)) {
+const radixToken = radixUniverse.nativeToken
+myAccount.transferSystem.getTokenUnitsBalanceUpdates().subscribe(balance => {
+  // Get the balance for the token we are interested in
+  const nativeTokenBalance = balance[radixToken.toString()]
+  console.log("nativeTokenBalance: " + nativeTokenBalance)
+  // do we have at least 5 tokens?
+  if (nativeTokenBalance.greaterThan(5)) {
 
-      // Put your friends' address here
-      const toAddress = 'JHn1iZFKf3GPwk7dMcsYRN9gG8BCSdsvQa8CBENuiwV69Y9pPCB'
-      const toAccount = RadixAccount.fromAddress(toAddress, true)
+    // Put your friends' address here
+    const toAddress = 'JHn1iZFKf3GPwk7dMcsYRN9gG8BCSdsvQa8CBENuiwV69Y9pPCB'
+    const toAccount = RadixAccount.fromAddress(toAddress, true)
 
-      // Send 5 tokens to the address
-      RadixTransactionBuilder
-        .createTransferAtom(myAccount, toAccount, radixToken, 5)
-        .signAndSubmit(myIdentity)
-    }
-  })
+    // Send 5 tokens to the address
+    RadixTransactionBuilder
+      .createTransferAtom(myAccount, toAccount, radixToken, 5)
+      .signAndSubmit(myIdentity)
+  }
+})
 
-document.getElementById('address').innerHTML = 'My address: '+ myAccount.getAddress();
+document.getElementById('address').innerHTML = 'My address: ' + myAccount.getAddress();
 
 const symbol = 'EXMP'
 const name = 'Example Coin'
 const description = 'My example coin'
 const granularity = 1
-const amount = 1000
+const amount = 100
 const iconUrl = 'http://a.b.com/icon.png'
 
 // creazione del token e ricerca in caso di creazione con successo
-new RadixTransactionBuilder().createTokenSingleIssuance(
+new RadixTransactionBuilder().createTokenMultiIssuance(
   myIdentity.account,
   name,
   symbol,
@@ -65,19 +65,43 @@ new RadixTransactionBuilder().createTokenSingleIssuance(
   .subscribe({
     next: status => { console.log(status) },
     complete: async () => {
+
       console.log('Token defintion has been created')
+
       // ricerca del token 
       radixTokenManager.getTokenDefinition(tokenReference).then(tokenDefinition => {
-        document.getElementById('token').innerHTML = 'Total supply of EXMP: ' + tokenDefinition.totalSupply
+        document.getElementById('tokenAmount').innerHTML = 'Total supply of EXMP: ' + tokenDefinition.totalSupply
       }).catch(error => {
         // Token wasn't found for some reason
         console.error('Token not found')
       })
+
+      // minting some tokens
+      const amountToMint = 5
+      RadixTransactionBuilder.createMintAtom(myIdentity.account, tokenReference, amountToMint)
+        .signAndSubmit(myIdentity)
+        .subscribe({
+          next: status => { console.log(status) },
+          complete: () => { console.log('Tokens have been minted') },
+          error: error => { console.error('Error submitting transaction', error) }
+        })
+
+      // burning some tokens
+      const amountToBurn = 10
+      RadixTransactionBuilder.createBurnAtom(myIdentity.account, tokenReference, amountToBurn)
+        .signAndSubmit(myIdentity)
+        .subscribe({
+          next: status => { console.log(status) },
+          complete: () => { console.log('Tokens have been burned') },
+          error: error => { console.error('Error submitting transaction', error) }
+        })
+
     },
     error: error => { console.error('Error submitting transaction', error) }
   })
 
 const tokenReference = `/${myIdentity.account.getAddress()}/${symbol}`
 console.log('Token reference' + tokenReference)
+
 
 
